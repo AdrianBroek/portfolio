300cf910ca1ce13eecc131db143e5514639dbe19
Adrianbro96@gmail.com <Adrianbro96@gmail.com>
2022-10-12 19:19:09 +0200

    update data info to projects

--- src/utils.js
+++ src/utils.js
@@ -7,28 +7,28 @@ import blucar from '../src/images/projectsBackgrounds2/blucar.png'
 const utilProjects = [
     {
         name: 'KrawczykRental',
-        description: 'Webstore made in prestashop, style made from graphic designer project',
+        description: 'Webstore made in prestashop. Used tech stack: js, jquery, tpl, smarty, scss',
         link: 'https://krawczykrental.pl/',
         image: krawczykrental,
         backgroundColor: '#000'
     },
     {
         name: 'Namileo',
-        description: 'Webstore made in prestashop, style made from graphic designer project',
+        description: 'Webstore made in prestashop. Used tech stack: js, jquery, tpl, smarty, scss',
         link: 'https://namileo.com/',
         image: namileo,
         backgroundColor: '#000'
     },
     {
         name: 'iPomocni',
-        description: 'Webstore made in Wordpress, style made from graphic designer project',
+        description: 'Webstore made in Wordpress mostly via creative elements.',
         link: 'https://ipomocni.pl/',
         image: iPomocni,
         backgroundColor: '#000'
     },
     {
         name: 'Blucar',
-        description: 'Webstore made in Wordpress, style made from graphic designer project, I had to use ACF',
+        description: 'Webstore made in Wordpress. WP Car rental theme, ACF',
         link: 'https://blucar.pl/',
         image: blucar,
         backgroundColor: '#000'
