582e956660a32d2417b21cf5ec33d435ca128fa5
Adrianbro96@gmail.com <Adrianbro96@gmail.com>
2022-10-24 21:12:49 +0200

    added lil animation in background

--- src/App.js
+++ src/App.js
@@ -11,6 +11,12 @@ import Skillset from './pages/Skillset'
 import Projects from './pages/Projects'
 import Contact from './pages/Contact'
 
+// icons
+import atom from './images/icons/atom.png'
+import htmlCoding from './images/icons/html-coding.png'
+import javaScript from './images/icons/java-script.png'
+import css from './images/icons/css-3.png'
+
 function App() {
   window.onbeforeunload = function () {
     window.scrollTo(0, 0);
@@ -22,6 +28,16 @@ function App() {
       <Skillset />
       <Projects />
       <Contact />
+      <div class="boxes">
+        <ul>
+          <li><img src={atom}></img></li>
+          <li><img src={css}></img></li>
+          <li><img src={htmlCoding}></img></li>
+          <li><img src={atom}></img></li>
+          <li><img src={htmlCoding}></img></li>
+          <li><img src={javaScript}></img></li>
+        </ul>
+      </div>
     </StateProvider>
   );
 }
